a8a6a860d0969e2ad1a12c530c967a8d0bfb7310
Mykola Melnyk <melnyk.cto.dev@gmail.com>
2022-06-07 18:08:36 +0300

    fix: do not repeat reviews

--- wp-content/themes/engineering-solutions/assets/js/geolocation.js
+++ wp-content/themes/engineering-solutions/assets/js/geolocation.js
@@ -2052,15 +2052,8 @@ const countries = [
 document.addEventListener('DOMContentLoaded', function () {
   const myLatLng = {lat: 50.450001, lng: 30.523333};
 
-  const svgMarker = {
-    path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-    fillColor: "blue",
-    fillOpacity: 0.6,
-    strokeWeight: 0,
-    rotation: 0,
-    scale: 2,
-    anchor: new google.maps.Point(15, 30),
-  };
+  // TODO: need fix url
+  const iconBase ='http://localhost/engineering-solutions/wp-content/uploads/2022/06/circle-e1654600134418.png';
 
   const map = new google.maps.Map(document.getElementById('google-map'), {
     center: myLatLng,
@@ -2088,7 +2081,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const allLocations = new google.maps.Marker({
       position: {lat: markers[i].lat, lng: markers[i].lng},
       map,
-      icon: svgMarker,
+      icon: iconBase,
       title: markers[i].country,
     });
 
